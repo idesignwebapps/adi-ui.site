cd76d663f6c7749ca28edd5935705d9340bd2812
Aditya Jain <66673800+idesignwebapps@users.noreply.github.com>
2024-01-22 20:07:40 +0000

    delete assets/content-management directory

--- assets/content-management/small-business-website-revisited/includes/navigation.inc.php
+++
@@ -1,9 +0,0 @@
-<!-- START Navigation -->
-<nav>
-    <ul>
-        <li><a href="./index.php">Home</a></li>
-        <li><a href="./groceries.php">Groceries</a></li>
-        <li><a href="./contact.php">Contact Us</a></li>
-    </ul>
-</nav>
-<!-- END Navigation -->
